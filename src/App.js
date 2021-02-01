dd4005f92d23c58dd55b72a6a042d2288b3f32b8
JuarezGoesJunior <juarezantoniog12@gmail.com>
2021-01-31 21:25:22 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import CharacterList from "./components/CharacterList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      characterList: [],
+    };
+  }
+
+  async componentDidMount() {
+    const result = await fetch("https://rickandmortyapi.com/api/character/");
+    const JSON = await result.json();
+    this.setState({ characterList: [...JSON.results] });
+  }
+
+  render() {
+    const { characterList } = this.state;
+    return (
+      <div className="App">
+        <CharacterList list={characterList} />
+      </div>
+    );
+  }
 }
 
 export default App;
